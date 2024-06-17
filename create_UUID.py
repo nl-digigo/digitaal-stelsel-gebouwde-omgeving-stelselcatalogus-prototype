9a098298a3d1552004eb8013d531586386570b1a
ConstantijnM-Digigo <139209860+ConstantijnM-Digigo@users.noreply.github.com>
2024-06-17 17:21:20 +0200

    debug

---
+++ create_UUID.py
@@ -0,0 +1,20 @@
+import uuid
+
+# Generate a random UUID
+uuid4 = uuid.uuid4()
+print(f"UUID4: {uuid4}")
+
+uuid4 = uuid.uuid4()
+print(f"UUID4: {uuid4}")
+
+uuid4 = uuid.uuid4()
+print(f"UUID4: {uuid4}")
+
+uuid4 = uuid.uuid4()
+print(f"UUID4: {uuid4}")
+
+uuid4 = uuid.uuid4()
+print(f"UUID4: {uuid4}")
+
+uuid4 = uuid.uuid4()
+print(f"UUID4: {uuid4}")
